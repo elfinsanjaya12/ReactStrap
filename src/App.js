bd26c3694a6255502d01eaa4473d6cdf65a3c08a
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-02-13 15:25:08 +0700

    Card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Media,
+  Alert,
+  Badge,
+  Breadcrumb,
+  ButtonGroup,
+  ButtonToolbar,
+  Card
+} from "react-bootstrap";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Breadcrumb>
+          <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+          <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+            Library
+          </Breadcrumb.Item>
+          <Breadcrumb.Item active>Data</Breadcrumb.Item>
+        </Breadcrumb>
+        <Row>
+          <Col>
+            <Media>
+              <img
+                width={64}
+                height={64}
+                className="mr-3"
+                src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png"
+                alt="Generic placeholder"
+              />
+              <Media.Body>
+                <h5>Media Heading</h5>
+                <p>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                  scelerisque ante sollicitudin commodo. Cras purus odio,
+                  vestibulum in vulputate at, tempus viverra turpis. Fusce
+                  condimentum nunc ac nisi vulputate fringilla. Donec lacinia
+                  congue felis in faucibus.
+                </p>
+              </Media.Body>
+            </Media>
+          </Col>
+          <Col>
+            <Media>
+              <img
+                width={64}
+                height={64}
+                className="mr-3"
+                src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png"
+                alt="Generic placeholder"
+              />
+              <Media.Body>
+                <h5>Media Heading</h5>
+                <p>
+                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                  scelerisque ante sollicitudin commodo. Cras purus odio,
+                  vestibulum in vulputate at, tempus viverra turpis. Fusce
+                  condimentum nunc ac nisi vulputate fringilla. Donec lacinia
+                  congue felis in faucibus.
+                </p>
+              </Media.Body>
+            </Media>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Alert variant="success">Success Login</Alert>
+          </Col>
+          <Col>
+            <h1>
+              <Badge variant="danger">New</Badge>
+            </h1>
+            <Button variant="primary">
+              Profile <Badge variant="light">9</Badge>
+            </Button>
+          </Col>
+          <Col>
+            <Button variant="danger">Klik Me</Button>
+            <br />
+            <ButtonToolbar aria-label="Toolbar with button groups">
+              <ButtonGroup className="mr-2" aria-label="First group">
+                <Button>1</Button>
+                <Button>2</Button>
+                <Button>3</Button>
+                <Button>4</Button>
+              </ButtonGroup>
+
+              <ButtonGroup className="mr-2" aria-label="Second group">
+                <Button>5</Button>
+                <Button>6</Button>
+                <Button>7</Button>
+              </ButtonGroup>
+
+              <ButtonGroup aria-label="Third group">
+                <Button>8</Button>
+              </ButtonGroup>
+            </ButtonToolbar>
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col>
+            <Card style={{ width: "300px" }}>
+              <Card.Img
+                variant="top"
+                src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png"
+              />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This card has supporting text below as a natural lead-in to
+                  additional content.{" "}
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>1</Col>
+          <Col>1</Col>
+        </Row>
+      </Container>
     );
   }
 }
